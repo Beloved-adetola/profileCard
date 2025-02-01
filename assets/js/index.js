48e48487021be9e8f8fd5fb2960c17beb60fcf60
Adetola Beloved <belovedadetola@gmail.com>
2025-02-01 12:58:02 +0100

    final commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,23 @@
 function updateTime() {
-    document.getElementById("time").textContent = new Date().toUTCString();
+  document.getElementById("time").textContent = new Date().toUTCString();
 }
 updateTime();
 setInterval(updateTime, 60000);
+
+function runTypingEffect() {
+  const text = "Front-end Developer.";
+  const typingElement = document.getElementById("typing-text");
+  const typingDelay = 80;
+
+  typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, typingDelay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(i);
+    }, typingDelay * i);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", runTypingEffect());
